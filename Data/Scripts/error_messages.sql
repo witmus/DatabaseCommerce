8b19337ecbf36e8e8cb7e86f8dac610c23ecc7ce
witmus <witekmuszynski123@wp.pl>
2024-06-03 20:45:47 +0200

    add missing error messages

--- Data/Scripts/error_messages.sql
+++ Data/Scripts/error_messages.sql
@@ -3,3 +3,18 @@ GO
 
 EXEC sys.sp_addmessage 50001,1,'category_not_found',@replace='replace'
 GO
+
+EXEC sys.sp_addmessage 50002,1,'user_not_found',@replace='replace'
+GO
+
+EXEC sys.sp_addmessage 50003,1,'product_not_found',@replace='replace'
+GO
+
+EXEC sys.sp_addmessage 50004,1,'invalid_product_amount',@replace='replace'
+GO
+
+EXEC sys.sp_addmessage 50005,1,'category_name_taken',@replace='replace'
+GO
+
+EXEC sys.sp_addmessage 50006,1,'cart_empty',@replace='replace'
+GO
